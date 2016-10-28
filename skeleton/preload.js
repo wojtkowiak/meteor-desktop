f12e159124d23df49e479a648fa95b4efe8036a8
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2016-10-28 21:10:47 +0200

    fix tests

--- skeleton/preload.js
+++ skeleton/preload.js
@@ -1,29 +1,5 @@
-// This is a modified version of
+// This was inspiried by
 // https://github.com/electron-webapps/meteor-electron/blob/master/app/preload.js
-
-/**
- The MIT License (MIT)
-
- Copyright (c) 2015 Michael Risse
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
- */
 
 /**
  * Since we've disabled Node integration in the Browser window, we must selectively expose
@@ -44,7 +20,7 @@ try {
     // If that fails, then probably this is production build and devtron is not available.
 }
 
-if (process.env.NODE_ENV === 'test' && window !== undefined) {
+if (process.env.NODE_ENV === 'test' && typeof window !== 'undefined') {
     window.electronRequire = require;
 }
 
