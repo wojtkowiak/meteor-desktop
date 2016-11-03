ffb27c83d588ad8dcf47e74aa78be15845904c58
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2016-11-03 12:37:52 +0100

    add integration test

--- tests/integration/integration.test.js
+++ tests/integration/integration.test.js
@@ -1,3 +1,4 @@
+// eslint-disable no-console
 import chai from 'chai';
 import dirty from 'dirty-chai';
 import sinonChai from 'sinon-chai';
@@ -5,77 +6,158 @@ import sinon from 'sinon';
 import fs from 'fs';
 import path from 'path';
 import shell from 'shelljs';
-import mockery from 'mockery';
+import electron from 'electron';
+import { Application } from 'spectron';
+
+import paths from '../helpers/paths';
+import meteorDesktop from '../../lib/index';
 
 shell.config.fatal = true;
 
-import paths from '../helpers/paths';
-
 chai.use(sinonChai);
 chai.use(dirty);
-const {describe, it} = global;
-const {expect} = chai;
+const { describe, it } = global;
+const { expect } = chai;
 
-//const meteorDesktop = require('../helpers/meteorDesktop');
+/**
+ * This is first experimental integration test.
+ * @type {string}
+ */
 
-import meteorDesktop from '../../lib/index';
-
-/*const {
-    createTestInstance, StubLog, getModuleJson, saveModuleJson
-} = meteorDesktop;*/
 
 let appDir = '';
+
+/**
+ * Waits until a promise from a function finally returns true.
+ * @param {Function} functionReturningPromise - function to test
+ * @param {number}   ms                       - expiration timeout in milliseconds
+ * @returns {Promise}
+ */
+function waitFor(functionReturningPromise, ms = 10000) {
+    return new Promise((resolve, reject) => {
+        let invokerTimeout;
+        let timeout;
+        const invokeFunction = () =>
+            functionReturningPromise()
+                .then((result) => {
+                    console.log(result);
+                    if (result) {
+                        clearTimeout(invokerTimeout);
+                        clearTimeout(timeout);
+                        resolve();
+                    } else {
+                        invokerTimeout = setTimeout(invokeFunction, 500);
+                    }
+                })
+                .catch(() => {
+                    invokerTimeout = setTimeout(invokeFunction, 500);
+                });
+        invokeFunction();
+        timeout = setTimeout(() => {
+            clearTimeout(invokerTimeout);
+            reject('timeout expired on waitFor');
+        }, ms);
+    });
+}
+
+/**
+ * Waits for the app to load and appear.
+ * @param {Object} app - test app
+ * @returns {{app: (Application|*), window: *}}
+ */
+async function waitForApp(app) {
+    await app.client.waitUntilWindowLoaded();
+    const window = app.browserWindow;
+    // Wait for the main window for max 30seconds. Adjust to your app.
+    await waitFor(window.isVisible, 30000);
+    expect(await app.client.getWindowCount()).to.equal(1);
+    await app.client.waitUntil(
+        async () => await app.client.execute(
+            () => document.readyState === 'complete'
+        )
+    );
+    return { app, window };
+}
 
 describe('desktop', () => {
     let MeteorDesktop;
 
-    before((done) => {
-        //shell.mkdir('-p', paths.testsTmpPath);
-        console.log('create');
-        //shell.exec('meteor create test-desktop --release=METEOR@1.4.1.3', { cwd:
-        // paths.testsTmpPath });
+    before(() => {
         appDir = path.join(paths.testsTmpPath, 'test-desktop');
-        /*
-        const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..',
-         'package.json'), 'utf8'));
-
-        const packages = Object.keys(packageJson.dependencies).map((dep) => `${dep}@${packageJson.dependencies[dep]}`).join(' ');
-        console.log(`npm install ${packages}`);
-        // NYC seems to mess with executing `npm install` or `meteor npm install` so we are
-        // pointing directly to npm-cli.js as a workaround.
-        const npmPath = path.join(appDir, 'node_modules', 'npm', 'bin', 'npm-cli.js');
-        shell.exec(`node ${npmPath} install ${packages}`, { cwd: appDir });
-        console.log('done npm');*/
-        done();
     });
 
     beforeEach(() => {
-        //MeteorDesktop = createTestInstance();
+        try {
+            fs.unlinkSync('meteor.log');
+        } catch (e) {
+            // No worries...
+        }
     });
 
     describe('add to scripts', () => {
-        it('should add a `desktop` entry in package.json', (done) => {
-            const exitStub = sinon.stub(process, 'exit');
-            require('../../lib/scripts/addToScripts');
+        let exitStub;
+        let cwdStub;
+        it('should add a `desktop` entry in package.json', () => {
+            exitStub = sinon.stub(process, 'exit');
+            cwdStub = sinon.stub(process, 'cwd');
+            cwdStub.returns(appDir);
+            require('../../lib/scripts/addToScripts'); // eslint-disable-line
             const packageJson = JSON.parse(fs.readFileSync(path.join(appDir, 'package.json'), 'utf8'));
+            expect(exitStub).to.have.callCount(0);
             expect(packageJson.scripts.desktop).to.be.equal('meteor-desktop');
-            expect(exitStub).to.not.be.called();
+        });
+
+        after(() => {
             exitStub.restore();
-            done();
+            cwdStub.restore();
         });
     });
 
-    describe('add to scripts', () => {
+    describe('build with params --init -b', () => {
+        let exitStub;
+        let app;
+
+        after(async () => {
+            exitStub.restore();
+            if (app && app.isRunning()) {
+                await app.stop();
+            }
+        });
+
         it('should create a build', async () => {
+            exitStub = sinon.stub(process, 'exit', () => {
+                try {
+                    console.log(fs.readFileSync('meteor.log', 'utf8'));
+                } catch (e) {
+                    // Nothing to do.
+                }
+                process.exit(1);
+            });
+            // Load plugins directly from the package instead of those published to atmosphere.
+            process.env.METEOR_PACKAGE_DIRS = path.resolve(path.join(__dirname, '..', '..', 'plugins'));
             MeteorDesktop = meteorDesktop(
                 appDir,
                 appDir,
-                { ddpUrl: 'http://127.0.0.1:3788', init: true, build: true }
+                { ddpUrl: 'http://127.0.0.1:3080', init: true, build: true }
             );
+            // Build the app.
             await MeteorDesktop.build();
+
+            // Run it.
+            app = new Application({
+                path: electron,
+                args: [path.join(appDir, '.meteor', 'desktop-build')],
+                requireName: 'electronRequire',
+                env: { NODE_ENV: 'test', ELECTRON_ENV: 'test', METEOR_DESKTOP_NO_SPLASH_SCREEN: 1 }
+            });
+            await app.start();
+            await waitForApp(app);
+
+            const title = await app.client.getTitle();
+            expect(title).to.equal('test-desktop');
+            const text = await app.client.getText('h1');
+            expect(text).to.equal('Welcome to Meteor!');
+
         }).timeout(10 * 60000);
     });
-
-
-
 });
--- tests/integration/prepare.js
+++ tests/integration/prepare.js
@@ -2,23 +2,21 @@ require('reify');
 const shell = require('shelljs');
 const path = require('path');
 const fs = require('fs');
-const paths = require('../helpers/paths.js').default;
+
+const testsPath = path.resolve(path.join(__dirname, '..', '..', 'tests'));
+const testsTmpPath = path.resolve(path.join(testsPath, '.__tmp'));
 
 shell.config.fatal = true;
-shell.rm('-rf', paths.testsTmpPath);
+shell.rm('-rf', testsTmpPath);
 console.log('creating test dir');
-shell.mkdir('-p', paths.testsTmpPath);
+shell.mkdir('-p', testsTmpPath);
 console.log('creating test meteor app');
-shell.exec('meteor create test-desktop --release=METEOR@1.4.2', { cwd: paths.testsTmpPath });
+shell.exec('meteor create test-desktop --release=METEOR@1.4.2', { cwd: testsTmpPath });
 
-let appDir = path.join(paths.testsTmpPath, 'test-desktop');
+const appDir = path.join(testsTmpPath, 'test-desktop');
 const packageJson = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'package.json'), 'utf8'));
-const packages = Object.keys(packageJson.dependencies).map((dep) => `${dep}@${packageJson.dependencies[dep]}`).join(' ');
+const packages = Object.keys(packageJson.dependencies).map(
+    dep => `${dep}@${packageJson.dependencies[dep]}`).join(' ');
 console.log(`npm install ${packages}`);
-// NYC seems to mess with executing `npm install` or `meteor npm install` so we are
-// pointing directly to npm-cli.js as a workaround.
-const npmPath = path.join(appDir, 'node_modules', 'npm', 'bin', 'npm-cli.js');
-//shell.exec(`node ${npmPath} install ${packages}`, { cwd: appDir });
-shell.exec(`meteor npm install install ${packages}`, { cwd: appDir });
-console.log('done npm');
-
+//shell.exec(`meteor npm install ${packages}`, { cwd: appDir });
+shell.exec(`meteor npm install ../../..`, { cwd: appDir });
