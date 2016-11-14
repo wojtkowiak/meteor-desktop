fcc92747742746fb57e1eea3c63b7c045588a4bc
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2016-11-15 00:21:58 +0100

    run integration tests on meteor 1.4.2.1

--- tests/integration/prepare.js
+++ tests/integration/prepare.js
@@ -11,7 +11,7 @@ shell.rm('-rf', testsTmpPath);
 console.log('creating test dir');
 shell.mkdir('-p', testsTmpPath);
 console.log('creating test meteor app');
-shell.exec('meteor create test-desktop --release=METEOR@1.4.2', { cwd: testsTmpPath });
+shell.exec('meteor create test-desktop --release=METEOR@1.4.2.1', { cwd: testsTmpPath });
 const appDir = path.join(testsTmpPath, 'test-desktop');
 console.log('npm install');
 shell.exec('meteor npm install ../../..', { cwd: appDir });
