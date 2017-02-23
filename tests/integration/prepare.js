ae221cf91361bb6c2eb8b6956fd706ae22a07932
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2017-02-23 10:25:39 +0100

    Be more verbose when preparing integration tests

--- tests/integration/prepare.js
+++ tests/integration/prepare.js
@@ -17,7 +17,9 @@ if (!fs.existsSync(testsTmpPath) || !fs.existsSync(path.join(appDir, 'package.js
     shell.mkdir('-p', testsTmpPath);
     console.log('creating test meteor app');
     shell.exec(`meteor create test-desktop --release=METEOR@${meteorVersion}`, { cwd: testsTmpPath });
+    console.log('installing meteor-desktop');
     shell.exec('npm install --save-dev ../../..', { cwd: appDir });
+    console.log('installing babel-runtime');
     shell.exec('npm install --save babel-runtime', { cwd: appDir });
 } else {
     const currentVersion = fs.readFileSync(path.join(appDir, '.meteor', 'release'), 'utf-8').split('@')[1].replace(/[\r\n]/gm, '');
@@ -25,12 +27,14 @@ if (!fs.existsSync(testsTmpPath) || !fs.existsSync(path.join(appDir, 'package.js
         console.log('updating meteor version');
         shell.exec(`meteor update --release=METEOR@${meteorVersion} --all-packages`, { cwd: appDir });
     }
-    shell.exec('npm prune', { cwd: appDir });
-    shell.exec('npm install', { cwd: appDir });
+    console.log('removing node_modules');
+    shell.rm('-rf', path.join(appDir, 'node_modules'));
+    console.log('meteor npm install');
+    shell.exec('meteor npm install', { cwd: appDir });
 }
 
 if (process.env.TRAVIS) {
     shell.config.fatal = false;
     shell.exec('meteor add-platform android', { cwd: appDir });
-    console.log(shell.exec('meteor build ../build --server=127.0.0.1:3000', { cwd: appDir }))
+    console.log(shell.exec('meteor build ../build --server=127.0.0.1:3000', { cwd: appDir }));
 }
