6420f0e23f79b04e3f3fcd4f2ba9f882fb36d778
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2017-03-02 16:12:36 +0100

    0.6.1

--- plugins/bundler/package.js
+++ plugins/bundler/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'omega:meteor-desktop-bundler',
-    version: '0.6.0',
+    version: '0.6.1',
     summary: 'Bundles .desktop dir into desktop.asar.',
     git: 'https://github.com/wojtkowiak/meteor-desktop',
     documentation: 'README.md'
