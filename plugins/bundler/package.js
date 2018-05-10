58b203f8fa2609c676abd12b8cfed50253f62827
Bartosz Wojtkowiak <bartosz@wojtkowiak.it>
2018-05-10 12:48:13 +0200

    Missing version bump

--- plugins/bundler/package.js
+++ plugins/bundler/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'omega:meteor-desktop-bundler',
-    version: '0.17.2',
+    version: '0.18.0',
     summary: 'Bundles .desktop dir into desktop.asar.',
     git: 'https://github.com/wojtkowiak/meteor-desktop',
     documentation: 'README.md'
