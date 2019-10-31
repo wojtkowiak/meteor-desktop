98b47705c0791eadbb6eff6fef9569d69470a145
Daniel Chaplin <black.danek@gmail.com>
2019-10-31 15:48:46 +0300

    bump version

--- plugins/bundler/package.js
+++ plugins/bundler/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'skadmin:meteor-desktop-bundler',
-    version: '2.1.1',
+    version: '2.2.0',
     summary: 'Bundles .desktop dir into desktop.asar.',
     git: 'https://github.com/sharekey/meteor-desktop',
     documentation: 'README.md'
