b3c2f4c4f31f9fccce3fd770288003e67ceb610c
Jan Dvorak <storyteller@freedombase.net>
2022-04-11 10:33:01 +0200

    Bump to v3

--- plugins/watcher/package.js
+++ plugins/watcher/package.js
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-arrow-callback */
 Package.describe({
     name: 'communitypackages:meteor-desktop-watcher',
-    version: '2.2.6',
+    version: '3.0.0',
     summary: 'Watches .desktop dir and triggers rebuilds on file change.',
     git: 'https://github.com/Meteor-Community-Packages/meteor-desktop',
     documentation: 'README.md',
